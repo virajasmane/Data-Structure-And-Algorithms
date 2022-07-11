c547cd2e34b28f60c5d04820a21b2b70dcb72eb5
virajasmane <virajasmane40@gmail.com>
2022-07-11 14:24:41 +0530

    DSA

---
+++ 8) Hashing/1) Count Distinct Elements.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Eg
+// I/P :- {1, 2, 2, 3, 4}
+// O/P :- 4
+
+// I/P :- {10, 10, 10}
+// O/P :- 1
+
+// Naive Solution
+int Count_Distinct_Elements(vector<int> vector)
+{
+  int No_of_Dist_elements = 0;
+
+  for(int i=0; i<vector.size(); i++)
+  {
+    bool isEqual = false;
+    for(int j=0; j<i; j++)
+    {
+      if(vector[i] == vector[j])
+        isEqual = true;
+    }
+    if(isEqual == false)
+      No_of_Dist_elements++;
+  }
+
+  return No_of_Dist_elements;
+}
+// Time Complexity :- O(n^2)
+// Aux Space :- O(1)
+
+
+//______________________________________________________________________________
+
+// Effecient Solution
+int E_Count_Distinct_Elements(vector<int> vector)
+{
+  unordered_set<int> container;
+
+  for(int i=0; i<vector.size(); i++)
+    container.insert(vector[i]);
+
+  return container.size();
+}
+//Time Complexity :- O(n)
+//Aux Space :- O(n)
+
+//______________________________________________________________________________
+
+// More Effecient Solution
+
+int E1_Count_Distinct_Elements(vector<int> vector)
+{
+  unordered_set<int> container(vector.begin(), vector.end());
+
+  return container.size();
+}
+//Time Complexity :- Theta(n)
+//Aux Space :- O(n)
+
+
+int main()
+{
+  int length;
+
+  cout<<"Length of Vector \n";
+  cin>>length;
+  vector<int> vector(length);
+
+  cout<<"Enter the elements of the Vector \n";
+  for(auto &i: vector)
+    cin>>i;
+
+  cout<<E1_Count_Distinct_Elements(vector);
+
+
+  return 0;
+}
